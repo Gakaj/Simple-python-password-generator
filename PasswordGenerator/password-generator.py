b390882226e2f40e16aa668a97ead1e8b36dfc91
Gakaj <samgedge8@gmail.com>
2026-04-21 19:35:45 +0100

    Added generator_without_length function

--- PasswordGenerator/password-generator.py
+++ PasswordGenerator/password-generator.py
@@ -1,1 +1,15 @@
-print("Testing commit")
+import random, string, time
+print("Auto length is 10 characters")
+lengthChoice = False
+
+
+punct = "*&^%$£@;:~#]{]}¬`!?<>\/_" #Set of punctuation that can be generated.
+chars = string.ascii_letters + punct # Set of possible characters (letters(a-Z) and punctuation)
+
+def generator_without_length(chars):
+    password = "".join(random.choice(chars) for i in range(10))
+    return password
+
+
+
+
